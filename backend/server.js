1330e66b196c534b6a1602535f336686a43d6026
Him7110 <yadavhimanshu7110@gmail.com>
2025-03-22 00:05:15 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import 'dotenv/config.js'  // to use envt variables
+import http from 'http';
+import app from './app.js';
+
+const port = process.env.PORT || 3000;
+
+
+const server = http.createServer(app);
+
+
+
+server.listen(port, () => {
+    console.log('Server is running on port 3000');
+}); 
+
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "../screens/Login";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div>Home</div>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<div>Register</div>} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes
