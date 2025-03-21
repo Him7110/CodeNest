33977e0593206123548e70c9086a73b6cf482d60
Him7110 <yadavhimanshu7110@gmail.com>
2025-03-22 01:26:20 +0530

    Day 2 Commits

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -17,6 +17,8 @@ export const createUserController = async (req, res) => {
 
         const token = user.generateJWT();
 
+        delete user._doc.password;
+
         res.status(201).json({ user, token});
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -46,6 +48,8 @@ export const loginController = async (req, res) => {
         }  
 
         const token = user.generateJWT();
+
+        delete user._doc.password;
 
         res.status(200).json({ user, token });
 
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Login from "../screens/Login";
+import Register from "../screens/Register";
+import Home from "../screens/Home";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<div>Register</div>} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     )
